667f83e4d7440a8b2a28a6a693a1b537d791b597
Lia <blue.star95@outlook.com>
2017-03-29 18:20:28 +0700

    Stratified sampling done

--- sample-codes/stratify.py
+++ sample-codes/stratify.py
@@ -1,31 +1,29 @@
+# Lia Ristiana
+# learning to do stratify random sampling on a dataset
+
 import pandas as pd
 import numpy as np
-
-# s = pd.Series(np.random.randn(50))
-# df = pd.DataFrame(np.random.randn(50, 4), columns=list('ABCD'))
 
 input_file="../datasets/iris.csv"
 
 df = pd.read_csv(input_file)
 
+# find out the class frequency
 classCount = df['class'].value_counts(sort=True)
 print("class frequency\n%s."%classCount)
 
+# DEFINE CLASS NAMES AS NEEDED
 classNames = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
 
-# df_stratify = df.loc[df['class'].isin(classNames)].sample(frac=0.1)
-
-df_stratify = list()
+df_stratified = pd.DataFrame()
 
 for name in classNames:
-	df_stratify.extend(df.loc[df['class']==name].sample(frac=0.1))
-	# print(df.loc[df['class']==name].sample(frac=0.1))
+	temp = df.loc[df['class']==name].sample(frac=0.1)
+	df_stratified = pd.concat([df_stratified,temp])
+	
+print("RESULT ARRAY")
+print(df_stratified.head(5))
 
-print(df.head(10))
-print(df_stratify)
-
-classCount = df_stratify['class'].value_counts()
+# find out the stratified sampling data frequency
+classCount = df_stratified['class'].value_counts()
 print("class frequency\n%s."%classCount)
-
-
-
