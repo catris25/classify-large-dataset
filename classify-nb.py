9ac3af111fa3422b45b5ec0544641328928f43c6
Lia <blue.star95@outlook.com>
2017-04-05 22:46:24 +0700

    nb classification

--- classify-nb.py
+++ classify-nb.py
@@ -15,20 +15,13 @@ start_time = time.time()
 
 dir_name = "2017-04-03 01:06:26"
 file_name = "2"
-input_file="results/clustered/%s/%s.csv"%(dir_name,file_name)
-df = pd.read_csv(input_file)
+input_file="results/training/%s/%s.csv"%(dir_name,file_name)
+df_training = pd.read_csv(input_file)
 
-# split data into training and testing set
-
-# training
-training_set = df.sample(frac=0.66, random_state=1)
-training_attr = training_set.ix[:,[0,1,2,3,4,5,6]]
-training_target = training_set.ix[:,7]
-
-# testing
-testing_set = df.loc[~df.index.isin(training_set.index)]
-testing_attr = testing_set.ix[:,[0,1,2,3,4,5,6]]
-testing_target = testing_set.ix[:,7]
+dir_name = "2017-04-03 01:06:26"
+file_name = "2"
+input_file="results/testing/%s/%s.csv"%(dir_name,file_name)
+df_testing = pd.read_csv(input_file)
 
 model = GaussianNB()
 model.fit(training_attr, training_target)
