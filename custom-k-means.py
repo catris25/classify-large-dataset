3aa2dd6b7e181b3edbf84eaf712c16d718fe63b8
Lia <blue.star95@outlook.com>
2017-04-28 02:50:33 +0700

    k means clustering

--- custom-k-means.py
+++ custom-k-means.py
@@ -19,9 +19,10 @@ input_file="results/sampled/%s/dataset.csv"%dir_name
 
 df = pd.read_csv(input_file)
 
-x = df.ix[:,[0,1,2,3,4,5,6]]
-x = x.values
-y = df.ix[:,7]
+x = df.ix[:,[0,1,2,3,4,5,6]].values
+y = df.ix[:,7].values
+
+k = 2
 
 # x_array = np.array([[10.979,7.748],
 #             [9.352,8.743],
@@ -34,7 +35,7 @@ y = df.ix[:,7]
 
 
 class K_Means:
-    def __init__(self, k=3, tol=0.001, max_iter=300):
+    def __init__(self, k=k, tol=0.001, max_iter=300):
         self.k = k
         self.tol = tol
         self.max_iter = max_iter
@@ -46,6 +47,7 @@ class K_Means:
             self.centroids[i] = data[i]
 
         for i in range(self.max_iter):
+            print("iteration: ",i)
 
             self.clustering = {}
 
@@ -53,9 +55,11 @@ class K_Means:
                 self.clustering[i] = []
 
             sum_distances = 0
+            self.labels =[]
             for featureset in data:
                 distances = [np.linalg.norm(featureset - self.centroids[centroid]) for centroid in self.centroids]
                 cluster_label = distances.index(min(distances))
+                self.labels.append(cluster_label)
                 self.clustering[cluster_label].append(featureset)
 
                 sum_distances += min(distances)
@@ -83,9 +87,6 @@ class K_Means:
 clr = K_Means()
 clr.fit(x)
 
-# for i in range(len(clr.clustering)):
-#     print(i,". ",clr.clustering[i])
-
 # SAVE THE DATA TO ITS OWN CSV BASED ON ITS CLUSTER
 clusterNames = list(range(0,k))
 
@@ -93,14 +94,23 @@ dir_name = "%sclusters"%k
 os.makedirs("results/clustered/%s"%dir_name, exist_ok=True)
 
 # store the centroids
+
 centroids = pd.DataFrame(clr.centroids)
-df_centroids = pd.DataFrame(centroids,columns=['attr1', 'attr2', 'attr3', 'attr4', 'attr5', 'attr6', 'attr7'])
+print("c:",centroids)
+
+centroids = np.transpose(centroids)
+df_centroids = pd.DataFrame(centroids)
+cols=['attr1', 'attr2', 'attr3', 'attr4', 'attr5', 'attr6', 'attr7']
+print("df:",df_centroids)
 df_centroids.to_csv("results/clustered/%s/centroids.csv"%dir_name, index=False)
-print("centroid:\n",centroids)
 print("SSE: ",clr.sse)
 
-for name in clustering:
-    temp = x.loc[x['cluster']==name]
+labels = clr.labels
+df["cluster"] = labels
+
+for name in clusterNames:
+    print("name:", name)
+    temp = df.loc[df['cluster']==name]
     temp.to_csv("results/clustered/%s/%s.csv"%(dir_name,name), index=False)
     classCount = temp['class'].value_counts(sort=False)
     print(classCount)
