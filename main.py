bb055d127aaf409804de79a1254cf063ffb07f71
Lia <blue.star95@outlook.com>
2017-05-06 16:41:06 +0700

    running test

--- main.py
+++ main.py
@@ -5,16 +5,16 @@
 # BUT IT WORKS FOR NOW, SO MAYBE I'D BETTER SHUT UP TOO
 
 # custom_k_means file --> cluster the data into smaller partition
-main_k = 2
+main_k = 8
 main_sampled_dir_name = "2017-05-04 23:23:02"
 import custom_k_means
 main_clustered_set_dir = custom_k_means.clustered_set_dir
 
 # testing_sampling --> sample the data from the main dataset for testing purposes
-main_n_testing_size = 1000
-import testing_sampling
-main_testing_set_dir = testing_sampling.testing_set_dir
-print("main_testing_set_dir",main_testing_set_dir)
+# main_n_testing_size = 1000
+# import testing_sampling
+# main_testing_set_dir = testing_sampling.testing_set_dir
+main_testing_set_dir = "results/testing/2017-05-05 01:47:07/testing-set.csv"
 
 # cluster-decision --> decide which cluster the testing set is closest to
 import cluster_decision
