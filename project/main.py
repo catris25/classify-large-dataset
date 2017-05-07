ef39fdea06157f373d8714b7d5178e6ce11d2af9
Lia <blue.star95@outlook.com>
2017-05-07 23:51:01 +0700

    running test

--- project/main.py
+++ project/main.py
@@ -2,31 +2,33 @@ import time
 start_time = time.time()
 
 # SAMPLING TRAINING SET
-print("# SAMPLING TRAINING SET")
-input_file="/home/lia/Documents/FINAL-GEMASTIK/training.csv"
-import sampling_trainingset as train
-training_sampled_dir = train.sampling(input_file)
-print("training sampled:", training_sampled_dir)
+# print("# SAMPLING TRAINING SET")
+# input_file="/home/lia/Documents/FINAL-GEMASTIK/training.csv"
+# import sampling_trainingset as train
+# training_sampled_dir = train.sampling(input_file)
+# print("training sampled:", training_sampled_dir)
 
 # CLUSTERING TRAINING SET
 print("# CLUSTERING TRAINING SET")
 import clustering_k_means as clsr
-input_file = training_sampled_dir
-k_size = 1
+# input_file = training_sampled_dir
+input_file = "output/training_sampled/2017-05-07 23:12:26/training_set.csv"
+k_size = 10
 training_clustered_dir = clsr.kmeans(input_file, k_size)
 print("training clustered:",training_clustered_dir)
 
 # SAMPLING TESTING SET
-print("# SAMPLING TESTING SET")
-input_file="/home/lia/Documents/FINAL-GEMASTIK/training.csv"
-import sampling_testingset as test
-n_size = 2000
-testing_sampled_dir = test.sampling(input_file, n_size)
-print("testing sampled:", testing_sampled_dir)
+# print("# SAMPLING TESTING SET")
+# input_file="/home/lia/Documents/FINAL-GEMASTIK/training.csv"
+# import sampling_testingset as test
+# n_size = 2000
+# testing_sampled_dir = test.sampling(input_file, n_size)
+# print("testing sampled:", testing_sampled_dir)
 
 # CLUSTERING TESTING SET
 print("# CLUSTERING TESTING SET")
-testing_file = testing_sampled_dir
+# testing_file = testing_sampled_dir
+testing_file = "output/testing_sampled/2017-05-07 23:12:32/testing-set.csv"
 training_clustered_file = training_clustered_dir
 import clustering_testingset as clsrtest
 testing_clustered_dir = clsrtest.cluster_test(training_clustered_file, testing_file)
@@ -48,6 +50,7 @@ n_size = 7
 tp_knn, all_knn = knn.classify_all(training_dir,testing_dir, k_size, n_size)
 
 # RESULT
+print("-------------------")
 print("# RESULT")
 print("cluster size =",k_size)
 print("Naive Bayes")
