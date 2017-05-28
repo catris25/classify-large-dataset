1c6645840a26766b509abd76e82307f911538c3b
Lia <blue.star95@outlook.com>
2017-05-28 22:51:13 +0700

    z test completed

--- project/z_test.py
+++ project/z_test.py
@@ -38,9 +38,15 @@ def calculate(df_population, df_sample):
         standard_error = math.sqrt((var1/n1)+(var2/n2))
         z_value = ((means1-means2)-d)/standard_error
         p_val = st.norm.sf(abs(z_value))*2
-        px = 1 - ss.ndtr(z_value)
-        pval = st.t.sf(np.abs(z_value),n2-1)*2
-        print("attr%s: %s, probability: %s, %s, %s"%(i,z_value,p_val,px, p_val))
+
+        # px = 1 - ss.ndtr(z_value)
+        # pval = st.t.sf(np.abs(z_value),n2-1)*2
+
+        # as it turns out the calculation of pval and p_val are the same
+        # and px is one tail
+
+        # print("attr%s: %s, probability: %s, %s, %s"%(i,z_value,p_val,px, pval))
+        print("attr-%s z = %s, p = %s"%(i,z_value, p_val))
 
         # calculate the degrees of freedom
         # pembilang = (var1/n1+var2/n2)**2
@@ -54,11 +60,12 @@ def main():
     input_file = "output/population_statistics.csv"
     df_population = pd.read_csv(input_file)
 
-    input_file = "output/training_sampled/2017-05-07 23:12:26/stats.csv"
+    input_file = "output/training_sampled/2017-05-28 22:50:21/training_set.csv"
     df_sample = pd.read_csv(input_file)
 
+    print("input file:",input_file)
     calculate(df_population, df_sample)
-    
+
     # print("test",st.norm.cdf(1.64))
     # print(st.norm.sf(abs(1.64))*2)
     # print("test",st.t.ppf(0.93339648357,999))
