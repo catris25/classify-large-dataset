e073497c8e022cc2c6cf14551a41a5b562d0e466
Lia <blue.star95@outlook.com>
2017-05-28 22:53:17 +0700

    running test

--- project/z_test.py
+++ project/z_test.py
@@ -60,7 +60,7 @@ def main():
     input_file = "output/population_statistics.csv"
     df_population = pd.read_csv(input_file)
 
-    input_file = "output/training_sampled/2017-05-28 22:50:21/training_set.csv"
+    input_file = "output/training_sampled/2017-05-28 22:44:46/training_set.csv"
     df_sample = pd.read_csv(input_file)
 
     print("input file:",input_file)
